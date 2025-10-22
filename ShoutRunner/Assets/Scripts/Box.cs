a24edd75a299732d604fe4b55e3e333b9fc0b5e7
yumamatsuo <h1202319019@hiratagakuen.onmicrosoft.com>
2025-10-22 12:29:23 +0900

    スクリプト修正

--- ShoutRunner/Assets/Scripts/Box.cs
+++ ShoutRunner/Assets/Scripts/Box.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    public VoiceSetting voice_setting;
+    private VoiceSetting voice_setting;//VoiceSettingスクリプト取得用
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //VoiceSettingスクリプトがついたオブジェクトを取得
+        voice_setting = FindFirstObjectByType<VoiceSetting>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (voice_setting.GetVoiceVolume >= 0.9f)
+        if (voice_setting != null && voice_setting.GetVoiceVolume >= 0.9f)
         {
             Destroy(gameObject);
         }
