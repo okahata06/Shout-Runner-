3867062aea621c20107e7d7ec3a854b6e6f35931
岡畑藍 <airuo0603@gmail.com>
2025-11-18 16:11:05 +0900

    音声認識の方法を変更。テキスト化をするため。 書き換えにより現在音声による移動不可。

--- ShoutRunner/Assets/Scripts/PlayerMove.cs
+++ ShoutRunner/Assets/Scripts/PlayerMove.cs
@@ -238,7 +238,6 @@ public class PlayerMove : MonoBehaviour
     //現在のアングルから指定アングルまで徐々に回転させる
     void RotateToAngle(VoiceToText.VoiceCommand direction)
     {
-        Debug.Log(time);
         //左
         if (direction== VoiceToText.VoiceCommand.ひだり&&!foward)
         {
@@ -293,7 +292,6 @@ public class PlayerMove : MonoBehaviour
         //移動処理をしていない状態=Null
         if (command != VoiceToText.VoiceCommand.Null)
             return;
-        Debug.Log(text);
 
         pos = tr.position;
         //方向転換など
