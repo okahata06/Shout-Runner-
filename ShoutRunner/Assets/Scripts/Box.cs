dad7cf504c2b893a6a489c67aab5da1cd47f6fb6
yumamatsuo <h1202319019@hiratagakuen.onmicrosoft.com>
2025-12-02 16:25:03 +0900

    BOX破壊修正

--- ShoutRunner/Assets/Scripts/Box.cs
+++ ShoutRunner/Assets/Scripts/Box.cs
@@ -13,17 +13,22 @@ public class Box : MonoBehaviour
     public GameObject woodEffectPrefab;
     public GameObject dustEffectPrefab;
 
+    private Transform Target;
+
     // Start is called before the first frame update
     void Start()
     {
         //VoiceSettingスクリプトがついたオブジェクトを取得
         voice_setting = FindFirstObjectByType<VoiceSetting>();
+
+        Target = Camera.main.gameObject.transform;
+        //Target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (voice_setting != null && voice_setting.GetVoiceVolume >= destroyVolume)
+        if (voice_setting != null && voice_setting.GetVoiceVolume >= destroyVolume && this.transform.position.z - Target.position.z <= 6)
             BreakBox();
     }
 
